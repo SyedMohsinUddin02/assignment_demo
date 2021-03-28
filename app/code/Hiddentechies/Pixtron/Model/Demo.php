d138bc0d1adc0a1ee7a47f5249e71f1992b9174c
SyedMohsinUddin02 <syed.mohsinuddin02@gmail.com>
2021-03-28 20:05:09 +0530

    First commit

---
+++ app/code/Hiddentechies/Pixtron/Model/Demo.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hiddentechies\Pixtron\Model;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientFactory;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ResponseFactory;
+use Magento\Framework\Webapi\Rest\Request;
+
+/**
+ * Class GitApiService
+ */
+class Demo
+{
+    /**
+     * API request URL
+     */
+    const API_REQUEST_URI = 'https://api.themoviedb.org/';
+
+    /**
+     * API request endpoint
+     */
+    const API_REQUEST_ENDPOINT = '3/';
+
+    /**
+     * @var ResponseFactory
+     */
+    private $responseFactory;
+
+    /**
+     * @var ClientFactory
+     */
+    private $clientFactory;
+
+    /**
+     * GitApiService constructor
+     *
+     * @param ClientFactory $clientFactory
+     * @param ResponseFactory $responseFactory
+     */
+    public function __construct(
+        ClientFactory $clientFactory,
+        ResponseFactory $responseFactory
+    ) {
+        $this->clientFactory = $clientFactory;
+        $this->responseFactory = $responseFactory;
+    }
+
+    /**
+     * Fetch some data from API
+     */
+    public function movie(): void
+    {
+        $repositoryName = '3/movie/top_rated';
+        $response = $this->doRequest($repositoryName);
+        $status = $response->getStatusCode(); // 200 status code
+        $responseBody = $response->getBody();
+        $responseContent = $responseBody->getContents(); // here you will have the API response in JSON format
+        // Add your logic using $responseContent
+
+        $truedata = json_decode($responseContent,true);
+        for ($i = 0; $i < count($truedata['results']); $i++) {
+            echo $truedata['results'][$i]['title'];
+        }
+        die();
+    }
+
+    /**
+     * Do API request with provided params
+     *
+     * @param string $uriEndpoint
+     * @param array $params
+     * @param string $requestMethod
+     *
+     * @return Response
+     */
+    private function doRequest(
+        string $uriEndpoint,
+        array $params = [
+            'query' => [
+               'api_key' => '5ac2a5685f512d8bfdeec30d78ee9d8f',
+               'language' => 'en-US',
+               'page' => '1'
+            ]
+         ],
+        string $requestMethod = Request::HTTP_METHOD_GET
+    ): Response {
+        /** @var Client $client */
+        $client = $this->clientFactory->create(['config' => [
+            'base_uri' => self::API_REQUEST_URI
+        ]]);
+
+        try {
+            $response = $client->request(
+                $requestMethod,
+                $uriEndpoint,
+                $params
+            );
+        } catch (GuzzleException $exception) {
+            /** @var Response $response */
+            $response = $this->responseFactory->create([
+                'status' => $exception->getCode(),
+                'reason' => $exception->getMessage()
+            ]);
+        }
+
+        return $response;
+    }
+}
---
+++ app/etc/env.php
@@ -0,0 +1,83 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '54bb75ecb357a81c66966fc0ce0cf9c9'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'compass_box',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'b7c_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'b7c_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => false
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Tue, 16 Mar 2021 20:38:56 +0000'
+    ]
+];
